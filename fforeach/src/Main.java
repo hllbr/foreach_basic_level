710adc42511c90d0a5e17354db216be8352848a0
Halil İbrahim KOÇAK <halibrahim.kocak@gmail.com>
2020-12-02 16:03:29 +0300

    Add files via upload

---
+++ fforeach/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+         String [] array ={"haki","aslan ağzı","eflatun"};
+         Deneme [] dene = {new Deneme("hllbr"),new Deneme("@prince"),new Deneme("h2lbr")};
+         for(Deneme s : dene){
+             s.yaz();
+         }
+         System.out.println("*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/");
+        for(String a :array){
+            System.out.println(a); 
+                
+    }
+
+}
+    
+}
